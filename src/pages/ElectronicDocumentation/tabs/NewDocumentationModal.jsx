8ccb1c9ea5b82c8879a5b44937eab94dec9c1b50
yousif <me8999109@gmail.com>
2026-05-15 23:46:49 +0300

    تحديثات جديدة

--- src/pages/ElectronicDocumentation/tabs/NewDocumentationModal.jsx
+++ src/pages/ElectronicDocumentation/tabs/NewDocumentationModal.jsx
@@ -19,10 +19,10 @@ import { STAMP_TEMPLATE } from "../../../components/Stamp/stampTemplate";
 import { STAMP_TEMPLATE_QR } from "../../../components/Stamp/stampTemplateـqrcode";
 
 // 💡 استخدمنا مسار الـ legacy ليقوم بتوليد ملف .js عادي يقبله أي سيرفر
-import pdfWorkerUrl from 'pdfjs-dist/legacy/build/pdf.worker.min.js?url';
+import PdfWorker from "pdfjs-dist/build/pdf.worker.min.mjs?worker";
 
-// 💡 2. نقوم بتعيين الرابط
-pdfjs.GlobalWorkerOptions.workerSrc = pdfWorkerUrl;
+// 💡 نمرر الـ Worker هنا كـ workerPort بدلاً من workerSrc
+pdfjs.GlobalWorkerOptions.workerPort = new PdfWorker();
 
 export default function NewDocumentationModal({ isOpen, onClose, onSuccess }) {
   const [file, setFile] = useState(null);
